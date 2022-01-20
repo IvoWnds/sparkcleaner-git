a5fe06b84521a1593f14a1ddb0ae80916ad033d4
Ivo <86738054+MrMittens321@users.noreply.github.com>
2022-01-20 13:55:24 +0100

    Changed README and rebuilt package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="sparkcleaner",
-    version="1.1.0",
+    version="1.1.1",
     author="IvoW",
     author_email="ivownds@gmail.com",
     description="cleaning functions for pyspark df",
